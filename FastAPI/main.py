faa90bbb0cd6c4470df84e0e0b3f2d5c711d83f4
Pedro Zanatta <pedrozanattascariot@gmail.com>
2023-11-18 00:56:50 -0300

    fastapi fixes

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -10,12 +10,15 @@ app = FastAPI()
 
 #app are allowed to call our fastapi ondly if they are running on those urls
 origins = [
-    'http://localhost:3000'
+    'http://localhost:3000',
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
 #pydantic model to validate our react app requests
@@ -48,7 +51,7 @@ db_dependency = Annotated[Session, Depends(get_db)]
 models.Base.metadata.create_all(bind=engine)
 
 #endpoints
-@app.post("/transactions", response_model=TransactionModel)
+@app.post("/transactions/", response_model=TransactionModel)
 async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db_transaction = models.Transaction(**transaction.dict())
     db.add(db_transaction)
@@ -56,7 +59,7 @@ async def create_transaction(transaction: TransactionBase, db: db_dependency):
     db.refresh(db_transaction)
     return db_transaction
 
-@app.get("/transactions", response_model=List[TransactionModel])
+@app.get("/transactions/", response_model=List[TransactionModel])
 async def get_transactions(db: db_dependency, skip: int = 0, limit: int = 100):
     transactions = db.query(models.Transaction).offset(skip).limit(limit).all()
     return transactions
